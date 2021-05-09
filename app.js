c8b77133cc6017d5f38abafaae59aac107c696c0
Wissem Missary <wissem.missary@gmail.com>
2021-05-09 23:59:04 +0200

    apisauce

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
-
-const app = express();
 
 mongoose.connect('mongodb+srv://nonqme:AvNOCHX1Vk94IxCS@projet6.yuqxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
-    useUnifiedTopology: true })
+    useUnifiedTopology: true,
+    useCreateIndex: true 
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +23,9 @@ app.use((req, res, next) => {
   });
 
 app.use(express.json());
+
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
